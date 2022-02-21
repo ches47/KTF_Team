de6eaff91777cba75449140e12283f4fde06995b
Ches <chesy47@gmail.com>
2022-02-21 19:51:06 +0200

    Header, Footer modules --added

--- gulpfile.js
+++ gulpfile.js
@@ -60,11 +60,12 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function() {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/**/*.scss', gulp.parallel('sass')); //Для моделей html/css/js-отдельных-Блоков
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
 
-gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync',))
 
 
 
