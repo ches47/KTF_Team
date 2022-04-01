cd05eace7e6760880b523ae90518d566a2ffc239
vladian95 <kvankvan1995@gmail.com>
2022-04-01 18:36:16 +0300

    fixed languages

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,27 @@
-"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+
+    // const burgerBtn = document.querySelector('.header__burger-menu');
+    // const menuList = document.querySelector('.menu');
+
+    // burgerBtn.addEventListener('click', () => {
+    //     menuList.classList.toggle('menu--active');
+    // });
+
+
+    // //languages
+    const headerLanguages = document.querySelector('.header__languages');
+    const headerLanguagesArrow = document.querySelector('.header__languages-arrow');
+
+    headerLanguages.addEventListener('click', () => {
+        headerLanguagesArrow.style.transform = "rotate(180deg)";
+    });
+
+
+
+
+
+
+
+});
+
+
